d15d9bb986d6f5ef3873d77bc32814a73fec3f07
Priscilla Correa <pris.pcd@gmail.com>
2023-10-10 17:17:12 -0300

    card created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Card from './components/Card';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Card width="300px" height="400px">
+        <h2>Meu Card</h2>
+        <button>Clique Aqui</button>
+      </Card>
     </div>
   );
 }
---
+++ src/components/Card.js
@@ -0,0 +1,20 @@
+import styled from '@emotion/styled';
+
+const StyledCard = styled.div`
+  background-color: #232323;
+  border-radius: 10px;
+  padding: 20px;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: ${(props) => props.width || 'auto'};
+  height: ${(props) => props.height || 'auto'};
+`;
+
+const Card = ({ width, height, children }) => {
+  return <StyledCard width={width} height={height}>{children}</StyledCard>;
+};
+
+export default Card;
